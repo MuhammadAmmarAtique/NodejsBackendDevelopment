78211bd116d9e27508651d327f3ea3fd69c90fa9
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-07-17 20:48:16 +0500

    completed backend of our first full stack app

--- 02_frontendBackendConnection/Backend/index.js
+++ 02_frontendBackendConnection/Backend/index.js
@@ -1,9 +1,35 @@
 import express from "express"; //to use import statement add ("type": "module") in package.json file
 const app = express();
-const port = 3000;
+const port = 3500;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id:1,
+      text:"hehe very funny"
+    },
+    {
+      id:2,
+      text:"hehe very funny"
+    },
+    {
+      id:3,
+      text:"hehe very funny"
+    },
+    {
+      id:4,
+      text:"hehe very funny"
+    },
+    {
+      id:5,
+      text:"hehe very funny"
+    },
+  ]
+  res.send(jokes);
 });
 
 app.listen(port, () => {
