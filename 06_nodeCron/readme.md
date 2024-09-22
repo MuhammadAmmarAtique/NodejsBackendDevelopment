3961417462662a626df40a09ee16bfc364b0684e
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-09-22 16:57:16 +0500

    project setup

---
+++ 06_nodeCron/readme.md
@@ -0,0 +1,1 @@
+Learning how to schedule tasks in node.js using node cron
---
+++ 06_nodeCron/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server started successfully on port ${port}`);
+});
