e5ee681189e1230daca4c952cf3b27edb07fcb19
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-11-26 20:56:29 +0500

    web socket connection between client and server

--- 07_socket.io/client/src/App.jsx
+++ 07_socket.io/client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
-export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => {
+  //It creates a WebSocket connection to the server running at http://localhost:3700.
+  const socket = io("http://localhost:3700");  
+  //The socket object allows us to send and receive real-time messages between the client and the server.
+
+  useEffect(()=>{
+    socket.on("connect",()=>{
+      console.log("Web socket connected");
+    })
+  },[])
+
+  return <div>App</div>;
+};
+
+export default App;
--- 07_socket.io/server/index.js
+++ 07_socket.io/server/index.js
@@ -8,17 +8,23 @@ const app = express();
 const server = createServer(app); // here we are converting express app to raw http server so it can work with Web Socket
 
 // WebSocket/Socket.IO Setup:- 
-const io = new Server(server); //using http server creating web socket server (io)
+const io = new Server(server,{
+  cors:{
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+}); //by using http server creating web socket server (io) + cors configuration is done here like this!
 
 io.on("connection", (socket) => {
   console.log("User Connected!");
-  console.log("Socket Id:", socket.io);
+  console.log("Socket Id:", socket.id);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
