ad291d8f668acfd6ad34de5027d15a5efac0a980
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-11-26 18:22:47 +0500

    Socket.IO Setup

--- 07_socket.io/server/index.js
+++ 07_socket.io/server/index.js
@@ -1,12 +1,24 @@
-import express from 'express'
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-const app = express()
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const server = createServer(app); // here we are converting express app to raw http server so it can work with Web Socket
+
+// WebSocket/Socket.IO Setup:- 
+const io = new Server(server); //using http server creating web socket server (io)
+
+io.on("connection", (socket) => {
+  console.log("User Connected!");
+  console.log("Socket Id:", socket.io);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
